19a6188f8c59c42b714cbd2baa8101bcc919192f
anthonyde98 <anthonyde98@gmail.com>
2021-12-08 18:13:32 -0400

    Se arreglaron algunas cosas para produccion.

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/credit-scoring-app-frontend'));
+app.use(express.static(__dirname + '/dist/credit-scoring-front-end'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/credit-scoring-app-frontend/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/credit-scoring-front-end/index.html'));
 });
 
 // default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8080);
